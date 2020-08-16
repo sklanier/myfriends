a33b3883cfdd395008e3469bca8420dccd3889d3
Steve Lanier <stevekentlanier@gmail.com>
2020-08-16 15:58:16 -0700

    added dependencies. initial commit after server setup.

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require('express');
 
 const app = express();
 
+//ENDPOINTS
+app.get('/', (req, res) => res.send('SHES GIVIN IT ALL SHES GAWT'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));

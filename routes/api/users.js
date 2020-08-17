337cc2f2f98aaa6a7f56539f72554548ce679346
Steve Lanier <stevekentlanier@gmail.com>
2020-08-17 10:22:35 -0700

    Monday initial. Refactored server.js and added body-parser middleware.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const User = require('../../models/User');
 const router = express.Router();
 
+// @route    GET api/users/test
+// @desc     Tests users route
+// @access   Public
 router.get('/test', (req, res) => res.json({msg: "Users Works"}));
+
+// @route    GET api/users/register
+// @desc     Register user
+// @access   Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+})
 
 module.exports = router;

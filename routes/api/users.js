5ef2c382da1108c1cc869784a2b0798d0e3de4b9
Steve Lanier <stevekentlanier@gmail.com>
2020-08-17 10:57:25 -0700

    added new user construction

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const User = require('../../models/User');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcrypt.js');
 
 // @route    GET api/users/test
 // @desc     Tests users route
@@ -12,6 +14,36 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"}));
 // @access   Public
 router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({email: 'Email already exists'});
+      } else {
+
+        const avatar = gravatar.url(req.body.email, {
+          r: '200',
+          r: 'pg',
+          d: 'mm'
+        })
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+
+      }
+    })
 })
 
 module.exports = router;
